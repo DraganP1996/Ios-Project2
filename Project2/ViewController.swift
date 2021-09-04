7478e7ed1f48d59ef337b40cc97857c07edb7d81
Dragan Petrovic <pdragan1996@gmail.com>
2021-09-05 00:01:28 +0200

    Initial Commit

---
+++ Project2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project2
+//
+//  Created by Dragan Petrovic on 9/5/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
